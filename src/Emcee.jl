70271a05e02c9ee5f94a86322c8c71bc45755e81
Daniel O'Malley <omalled@lanl.gov>
2016-09-26 17:30:35 -0600

    initial commit -- basically working

---
+++ src/Emcee.jl
@@ -0,0 +1,51 @@
+module Emcee
+
+import RobustPmap
+
+function sample(llhood, numwalkers, x0, numsamples_perwalker, thinning, a=2.)
+	x = copy(x0)
+	chain = Array(Float64, size(x0, 1), numwalkers, div(numsamples_perwalker, thinning))
+	lastllhoodvals = RobustPmap.rpmap(llhood, map(i->x[:, i], 1:size(x, 2)))
+	llhoodvals = Array(Float64, numwalkers, numsamples_perwalker)
+	llhoodvals[:, 1] = lastllhoodvals
+	chain[:, :, 1] = x0
+	batch1 = 1:div(numwalkers, 2)
+	batch2 = div(numwalkers, 2)+1:numwalkers
+	divisions = [(batch1, batch2), (batch2, batch1)]
+	for i = 1:numsamples_perwalker
+		for ensembles in divisions
+			active, inactive = ensembles
+			zs = map(u->((a - 1) * u + 1) ^ 2 / a, rand(length(active)))
+			proposals = map(i->zs[i] * x[:, active[i]] + (1 - zs[i]) * x[:, rand(inactive)], 1:length(active))
+			newllhoods = RobustPmap.rpmap(llhood, proposals)
+			for (j, walkernum) in enumerate(active)
+				z = zs[j]
+				newllhood = newllhoods[j]
+				proposal = proposals[j]
+				logratio = (size(x, 1) - 1) * log(z) + newllhood - lastllhoodvals[walkernum]
+				if log(rand()) < logratio
+					lastllhoodvals[walkernum] = newllhood
+					x[:, walkernum] = proposal
+				end
+				if i % thinning == 0
+					chain[:, walkernum, div(i, thinning)] = x[:, walkernum]
+					llhoodvals[walkernum, div(i, thinning)] = lastllhoodvals[walkernum]
+				end
+			end
+		end
+	end
+	return chain
+end
+
+function flatten(chain)
+	numdims, numwalkers, numsteps = size(chain)
+	newchain = Array(Float64, numdims, numwalkers * numsteps)
+	for j = 1:numsteps
+		for i = 1:numwalkers
+			newchain[:, i + (j - 1) * numwalkers] = chain[:, i, j]
+		end
+	end
+	return newchain
+end
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,35 @@
+using Base.Test
+import Emcee
+
+srand(0)
+
+numdims = 5
+numwalkers = 100
+thinning = 10
+numsamples_perwalker = 1000
+burnin = 100
+
+function testit()
+	const stds = exp(5 * randn(numdims))
+	const means = 1 + 5 * rand(numdims)
+	llhood = x->begin
+		retval = 0.
+		for i in 1:length(x)
+			retval -= .5 * ((x[i] - means[i]) / stds[i]) ^ 2
+		end
+		return retval
+	end
+	x0 = rand(numdims, numwalkers) * 10 - 5
+	chain = Emcee.sample(llhood, numwalkers, x0, burnin, 1)
+	chain = Emcee.sample(llhood, numwalkers, chain[:, :, end], numsamples_perwalker, thinning)
+	flatchain = Emcee.flatten(chain)
+	for i = 1:numdims
+		@test_approx_eq_eps mean(flatchain[i, :]) means[i] 0.1 * stds[i]
+		@test_approx_eq_eps std(flatchain[i, :]) stds[i] 0.1 * stds[i]
+	end
+end
+
+for _ in 1:10
+	testit()
+end
+:passed
